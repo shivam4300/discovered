68d5dedfdefd0b7892a49332bd5acd96fc7b0113
shivam4300 <117337527+shivam4300@users.noreply.github.com>
2025-11-24 12:22:38 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf /home/my-project/public_html-deploy/.htaccess /home/my-project/public_html-deploy/.git /home/my-project/public_html-deploy/.vscode /home/my-project/public_html-deploy/.hintrc /home/my-project/public_html-deploy/.gitignore /home/my-project/public_html-deploy/.gitlab-ci.yml 
-rsync -avzp --exclude=appspec.yml --exclude=after_install.sh /home/my-project/public_html-deploy/ /home/my-project/public_html/
+rm -rf /home/shivam/public_html-deploy/.htaccess /home/shivam/public_html-deploy/.git /home/shivam/public_html-deploy/.vscode /home/shivam/public_html-deploy/.hintrc /home/shivam/public_html-deploy/.gitignore /home/shivam/public_html-deploy/.gitlab-ci.yml 
+rsync -avzp --exclude=appspec.yml --exclude=after_install.sh /home/shivam/public_html-deploy/ /home/shivam/public_html/
